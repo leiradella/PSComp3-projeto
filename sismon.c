4b666c4e4f4a94d709abdc6127a77d1fd56520ea
Lucas Leiradella <lucas@leiradella.com>
2024-03-03 16:23:21 +0000

    added makefile

--- sismon.c
+++ sismon.c
@@ -4,16 +4,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NS 3 /* numero de sectores a controlar */
-#define PSEN 5 /* perıodo inicial dos processos sensores (em segundos) */
-#define PACT 8 /* perıodo inicial dos processos actuadores (segundos) */
-#define PAMB 10 /* perıodo inicial dos processos ambiente (em segundos) */
-#define TINI 25 /* temperatura inicial dos sectores (em graus) */
-#define TMIN -100 /* temperatura minima inicial para alarme (em graus) */
-#define TMAX +100 /* temperatura maxima inicial para alarme (em graus) */
-#define NCICL 12 /* numero de ciclos para alternancia ambiente */
-#define NT 3   /*numero de threads*/
-
 struct Threadinputs
      {
     int TEMP;
@@ -112,7 +102,8 @@ void* thread_amb (void *threadinput)
     }
 }
 
-void main ()
+
+void main()
 {   
      threadinput.TEMP = TINI;
     pthread_t  threads[NT];
