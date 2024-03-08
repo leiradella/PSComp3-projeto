3142a865318e838cc2818d6389f7d13d7768330c
MFernandes25 <149201308+MFernandes25@users.noreply.github.com>
2024-03-08 00:02:27 +0000

    bruh

--- server/sismon.c
+++ server/sismon.c
@@ -1,9 +1,15 @@
 #include "naosei.h"
 #include "server_socket.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 int main ()
 {   
   char buf[100];
+  int i;
+  int f= 0;
   
   //criacao do socket
   serversocket servsock;
@@ -11,38 +17,53 @@ int main ()
   sock_create(&servsock);
 
   // criacao das threads
-  threadinput *threadinput1;
-  pthread_t  threads[NT];
+  threadinput *threadinput[NS];
+  pthread_t threads[NT*NS];
 
-  threadinput1 = (struct Threadinputs *)malloc(sizeof(struct Threadinputs));
-    if (threadinput1 == NULL){
+for (i = 0; i < 3; i++)
+{
+  threadinput[i] = (struct Threadinputs *)malloc(sizeof(struct Threadinputs));
+    if (threadinput[i] == NULL){
       printf("Erro ao alocar memoria\n");
-      return -1;
+      return EXIT_FAILURE;
     }
+}
+for(i = 0; i > 3
+  threadinput[0]->TEMP = TINI;
+  threadinput[0]->tmanip = 0;
+  threadinput[1]->TEMP = TINI;
+  threadinput[1]->tmanip = 0;
+  threadinput[2]->TEMP = TINI;
+  threadinput[2]->tmanip = 0;
+  
+while (f < NS*NT)
+{
 
-  threadinput1->TEMP = TINI;
-  threadinput1->tmanip = 0;
-
-  if (pthread_create(&threads[0], NULL, thread_sen, (void*)threadinput1) != 0) 
+for (i = 0; i < NS; i++)
+{
+  if (pthread_create(&threads[f], NULL, thread_sen, (void*)threadinput[i]) != 0) 
   {
     printf("Erro ao criar thread sensor"); 
   }
-  
-  if (pthread_create(&threads[1], NULL, thread_act, (void*)threadinput1) != 0) 
+  f++;
+  if (pthread_create(&threads[f], NULL, thread_act, (void*)threadinput[i])  != 0) 
   {
     printf("Erro a criar thread atuador"); 
   }
- 
-  if (pthread_create(&threads[2], NULL, thread_amb, (void*)threadinput1) != 0) 
+  f++;
+  if (pthread_create(&threads[f], NULL, thread_amb, (void*)threadinput[i])  != 0) 
   {
     printf("Erro a criar thread ambiente"); 
   }
-      
+  f++;
+ }   
+ }
   while (1)
   { 
     if (recvfrom(servsock.sd, buf, sizeof(buf), 0, (struct sockaddr *)&servsock.from, &servsock.fromlen) < 0) {
     perror("Erro no recvfrom");
     }
+    printf("%s", buf);
     if (strcmp(buf, "server_exit") == 0)
     {
       break;
