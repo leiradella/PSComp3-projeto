bf3931fe161031f36c69af9b9dc5f11aa5114c79
leiradella <lucas@leiradella.com>
2024-03-18 00:30:15 +0000

    Now intuti knows when to send messages to sismon or reghist

--- user/intuti.c
+++ user/intuti.c
@@ -1,19 +1,20 @@
 #include "client_socket.h"
+#include <string.h>
 #include <pthread.h>
 #include <unistd.h>
 #include <stdio.h>
 
-#define WORDSIZE 100
+#define MAXMESSAGESIZE 200
 #define NCOMMANDS (sizeof(commands)/sizeof(struct command_d))
 
 void *thread_func(void *clisoc)
 {
-    char buf [WORDSIZE];
+    char buf[MAXMESSAGESIZE];
     clientsocket *soc;
     soc = (clientsocket *)clisoc;
     while(1)
     {
-        if (recvfrom(soc->sd, buf, WORDSIZE, 0, NULL, NULL) < 0) {
+        if (recvfrom(soc->sd, buf, MAXMESSAGESIZE, 0, NULL, NULL) < 0) {
             perror("Erro no recvfrom");
         }
         printf("%s\n", buf);
@@ -42,11 +43,149 @@ struct 	command_d {
   {"lreg s [t1 [t2]]", "                    -lista registosdo setor s (0 = todos setores) entre t1 e t2, caso t2 nao seja especificado, lista todos desde t1 até o presente"}
 };
 
+void send_command(char *command, clientsocket clisoc)
+{
+    char *token = NULL;
+    char *message = NULL;
+    char *arg1;
+    int argc = 0;
+    int i = 0;
+
+    //do nothing if user just presses space
+    if (strcmp(command, "\n") == 0)
+    {
+        return;
+    }
+
+    //remove \n from the string
+    command[strlen(command)-1] = '\0';
+
+    //get space for token
+    token = (char *)malloc(sizeof(command)*sizeof(char));
+    if (token == NULL){
+      printf("Erro ao alocar memoria\n");
+      return;
+    }
+
+    //get space for arg1
+    arg1 = (char *)malloc(sizeof(command)*sizeof(char));
+    if (arg1 == NULL){
+      printf("Erro ao alocar memoria\n");
+      return;
+    }
+
+    //get space for message
+    message = (char *)malloc(sizeof(command)*sizeof(char)+2);
+    if (message == NULL){
+      printf("Erro ao alocar memoria\n");
+      return;
+    }
+
+    //clean message
+    strcpy(message, "");
+
+    //get the fist word (command) and put it in message and arg1
+    token = strtok(command, " ");
+    strcpy(message, token);
+    strcpy(arg1, token);
+
+    //1st cycle because of the space lol
+    token = strtok(NULL, " ");
+    argc++;
+    //this is to make the message format standard. for example if command is "dala      2   3     " message will be "dala 2 3".         
+    while(token != NULL)
+    {
+        argc++;
+        strcat(message, " ");
+        strcat(message, token);
+        token = strtok(NULL, " ");
+    }
+    free(token);
+
+    // after all this we have:
+    // argc with the number of args for switch case 
+    // message with a standard format of "command argument argument ... argument argc"
+    // arg1 which has just the command for strcmp
+
+    switch (argc) {
+        case 1:
+            if(strcmp(arg1, "tsm") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            else if(strcmp(arg1, "sair") == 0)
+            {
+                printf("Client closing...\n");
+                exit(0);        
+            }
+            else if(strcmp(arg1, "cala") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            else if(strcmp(arg1, "sos") == 0 || strcmp(arg1, "help") == 0)
+            {
+                for(i = 0; i < (int)NCOMMANDS; i++)
+                {
+                    printf("%s %s\n", commands[i].cmd_name, commands[i].cmd_help);
+                }
+                break;
+            }
+            printf("Intuti: comando invalido (argc = %d)\n", argc);
+            break;
+        case 2:
+            if(strcmp(arg1, "cps") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            else if(strcmp(arg1, "cts") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            printf("Intuti: comando invalido (argc = %d)\n", argc);
+            break;
+        case 3:
+            if(strcmp(arg1, "mpps") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            else if(strcmp(arg1, "mppa") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            else if(strcmp(arg1, "mppamb") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            else if(strcmp(arg1, "dala") == 0)
+            {
+                if (sendto(clisoc.sd, message, strlen(message)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+                break;
+            }
+            printf("Intuti: comando invalido (argc = %d)\n", argc);
+            break;
+        case 4:
+            printf("Intuti: comando invalido (argc = %d)\n", argc);
+            break;
+        default:
+            printf("Intuti: comando invalido (argc = %d)\n", argc);
+            break;
+    }
+
+    free(message);
+    free(arg1);
+}
+
 int main()
 {
     clientsocket clisoc;
     pthread_t  thread;
-    int i;
     char buf[50];
 
     create_client_socket(&clisoc);
@@ -60,14 +199,7 @@ int main()
     while(1)
     {
         fgets(buf, 50, stdin);
-        if(strcmp(buf, "sair\n") == 0) break;
-        else if((strcmp(buf, "sos\n") == 0) || (strcmp(buf, "help\n") == 0)) {
-            printf("lista de commandos\n");
-            for(i = 0; i < NCOMMANDS; i++)
-            {
-                printf("%s %s\n", commands[i].cmd_name, commands[i].cmd_help);
-            }
-        } else if (sendto(clisoc.sd, buf, strlen(buf)+1, 0, (struct sockaddr *)&clisoc.sis, clisoc.sislen) < 0) perror("CLI: Erro no sendto");
+        send_command(buf, clisoc);
     }
     
     close(clisoc.sd);
